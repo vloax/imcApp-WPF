e8fb036a0adf308d0c18474949b475efeac324f0
vitords.mello@gmail.com <84461857+vloax@users.noreply.github.com>
2023-04-17 21:30:54 -0400

    add: regras de negocio and change

--- imcApp-WPF/MainWindow.xaml.cs
+++ imcApp-WPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,11 +32,28 @@ namespace imcApp_WPF
         {
             var userAge = Convert.ToInt16(txtIdade.Text);
             var userHeight = Convert.ToInt32(txtAltura.Text);
+
+            if (userHeight > 240)
+            {
+                MessageBox.Show("Altura maior do que a aceitada, digite novamente", "Erro - ALTURA!");
+                txtPeso.Focus();
+                txtPeso.Clear();
+                txtIdade.Clear();
+                txtAltura.Clear();
+            }
+            
             var userWeight = Convert.ToInt32(txtPeso.Text);
 
             var imcCalc = userWeight / (userHeight * userWeight);
-        
+
+            var userGender = "Masculino";
+
+            if (btnFeminino.IsPressed)
+            {
+
+            }
              
+ 
         }
     }
 }
